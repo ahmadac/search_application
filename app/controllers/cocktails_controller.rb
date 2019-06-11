56ba9762dec3af50d218533b81e494cfee633367
Muhammad Ahmad <muhammadahmada188@gmail.com>
2019-06-11 18:16:05 +0500

    first commit

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,12 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+    @search = params["search"]
+    if @search.present?
+      @name = @search["name"]
+      # @cocktails = Cocktail.where(name: @name)
+      # @cocktails = Cocktail.where('name ILIKE ?', @name)
+      @cocktails = Cocktail.where('name ILIKE ?',  "%#{@name}%")
+    end
+  end
+end
